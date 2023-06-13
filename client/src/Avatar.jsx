146deef247e8a089e4b7fe7f503c1e6bf43eab54
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2023-06-12 21:26:38 -0700

    manejo errores

--- client/src/Avatar.jsx
+++ client/src/Avatar.jsx
@@ -1,9 +1,15 @@
+import PropTypes from 'prop-types';
 export default function Avatar({userId,username}) {
       const colors = ['bg-teal-200', 'bg-red-200',
                   'bg-green-200', 'bg-purple-200',
                   'bg-blue-200', 'bg-yellow-200',
                   'bg-orange-200', 'bg-pink-200', 'bg-fuchsia-200', 'bg-rose-200'];
-       const userIdBase10 = parseInt(userId.substring(10), 16);            
+       const userIdBase10 = parseInt(userId.substring(10), 16);   
+       
+       Avatar.propTypes = {
+    userId: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+  };
   return(
     <div className="w-8 h-8 bg-red-200 rounded-full flex items-center">
        <div className="text-center w-full">{
@@ -11,4 +17,6 @@ export default function Avatar({userId,username}) {
         </div>
     </div> 
   );
+
+ 
 }
